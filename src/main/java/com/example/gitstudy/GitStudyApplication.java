d727e228283c23379c37b8072913a2515b30945c
LEEDAEJIN <ldj4241@naver.com>
2024-08-07 23:34:40 +0900

    first change

--- src/main/java/com/example/gitstudy/GitStudyApplication.java
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitStudyApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("new line");
 		SpringApplication.run(GitStudyApplication.class, args);
 	}
 
